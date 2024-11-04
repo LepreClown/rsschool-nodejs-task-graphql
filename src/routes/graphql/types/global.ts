ed0be9c6f17972afbbfe0e61b59117e59499fc7d
Ivan Serpuhov <i.serpukhov@atomgroup.io>
2024-11-04 22:25:55 +0300

    refactor: Removed unused code

--- src/routes/graphql/types/global.ts
+++ src/routes/graphql/types/global.ts
@@ -1,8 +1,7 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
 export enum ERRORS {
-  INVALID_QUERY = "Query is invalid",
-  SERVER_ERROR_500 = "500 Internal Server Error",
+  SERVER_ERROR_500 = '500 Internal Server Error',
 }
 
 export interface Context {
